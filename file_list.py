e2879c00e4b78ecae16b4d7d7840554a27ff67d5
aliguk <gukov0aleksey@gmail.com>
2020-06-29 17:13:11 +0300

    change home_dir logs

--- file_list.py
+++ file_list.py
@@ -1,10 +1,10 @@
 filename_list = [
-    ["logs_input/s530_obr1_45let.log", ""],
-    ["logs_input/s531_obr1_65let.log", ""],
-    ["logs_input/s532_obr2_45let.log", ""],
-    ["logs_input/s533_obr2_65let.log", ""],
-    ["logs_input/s530_obr1_45let_small.log", ""],
-    ["logs_input/s531_obr1_65let_small.log", ""],
-    ["logs_input/s532_obr2_45let_small.log", ""],
-    ["logs_input/s533_obr2_65let_small.log", ""],
+    ["_logs_input/s530_obr1_45let.log", ""],
+    ["_logs_input/s531_obr1_65let.log", ""],
+    ["_logs_input/s532_obr2_45let.log", ""],
+    ["_logs_input/s533_obr2_65let.log", ""],
+    ["_logs_input/s530_obr1_45let_small.log", ""],
+    ["_logs_input/s531_obr1_65let_small.log", ""],
+    ["_logs_input/s532_obr2_45let_small.log", ""],
+    ["_logs_input/s533_obr2_65let_small.log", ""],
 ]
