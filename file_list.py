25c621230d2717231a05abcd0526eafb94bf1a0f
aliguk <gukov0aleksey@gmail.com>
2020-07-03 09:48:26 +0300

    add files_in

--- file_list.py
+++ file_list.py
@@ -7,4 +7,6 @@ filename_list = [
     ["_logs_input/s531_obr1_65let_small.log", ""],
     ["_logs_input/s532_obr2_45let_small.log", ""],
     ["_logs_input/s533_obr2_65let_small.log", ""],
+    ["_logs_input/s655_obr2_20let.log", ""],
+    ["_logs_input/s656_obr2_20let_60deg.log", ""],
 ]
